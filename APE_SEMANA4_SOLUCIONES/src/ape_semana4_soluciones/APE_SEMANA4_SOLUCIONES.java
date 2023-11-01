c049a907c0e21edfe7720e678f8ee1fb46a217fc
nayecast <nacastillo11@utpl.edu.ec>
2023-11-01 16:30:53 -0500

    area tringulo

---
+++ APE_SEMANA4_SOLUCIONES/src/ape_semana4_soluciones/APE_SEMANA4_SOLUCIONES.java
@@ -0,0 +1,13 @@
+/*
+*/
+package ape_semana4_soluciones;
+public class APE_SEMANA4_SOLUCIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
